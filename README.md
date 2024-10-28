4bbb0088516a7fa610e264f03d7eeac6aa723826
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-28 15:18:29 +0100

    updated deployment process

--- README.md
+++ README.md
@@ -14,40 +14,6 @@ chmod +x deploy.sh
 ./deploy.sh
 ```
 
-
-### Manual Deployment:
-Install node modules:
-```bash
-npm install --prefix ./src
-```
-
-Deploy the application using SAM:
-Note: Keep in mind that npm, aws cli and samcli has to be installed on your system
-```bash
-sam build
-
-# Set variables
-AWS_DEFAULT_REGION="us-east-1"
-
-ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-RANDOM_STRING=$(openssl rand -base64 6 | tr -dc 'a-z0-9' | head -c 6)
-S3_BUCKET_NAME="discordcdnservicelambda-$ACCOUNT_ID-${AWS_DEFAULT_REGION//-/}-$RANDOM_STRING"
-
-DISCORD_BOT_TOKEN="token"
-DISCORD_CHANNEL_ID="id"
-
-# Create the S3 bucket
-aws s3 mb s3://$S3_BUCKET_NAME --region $AWS_DEFAULT_REGION
-
-# Deploy the SAM application
-sam deploy --template-file template.yml \
-  --stack-name discordCDNServiceLambdaStack \
-  --capabilities CAPABILITY_IAM \
-  --s3-bucket "$S3_BUCKET_NAME" \
-  --region "$AWS_DEFAULT_REGION" \
-  --parameter-overrides DiscordBotToken=$DISCORD_BOT_TOKEN DiscordChannelId=$DISCORD_CHANNEL_ID
-```
-
 ## Cleanup:
 
 Just run these two commands:
@@ -56,12 +22,5 @@ Just run these two commands:
 # Set variables
 AWS_DEFAULT_REGION="us-east-1"
 
-ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-S3_BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[?starts_with(Name, \`discordcdnservicelambda-${ACCOUNT_ID}-${AWS_DEFAULT_REGION//-/}-\`)].Name" --output text)
-
 # Delete SAM Stack
 aws cloudformation delete-stack --stack-name discordCDNServiceLambdaStack --region $AWS_DEFAULT_REGION
-
-# Delete S3 Bucket
-aws s3 rb s3://$S3_BUCKET_NAME --force --region $AWS_DEFAULT_REGION
-```
--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,35 @@ AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
 
 read -p "Enter your Discord Bot Token: " DISCORD_BOT_TOKEN
 read -p "Enter your Discord Channel ID: " DISCORD_CHANNEL_ID
-read -p "Enter your desirec CDN URL eg. https://cdn.example.com : " CDN_URL
+read -p "Enter your CDN URL (e.g. https://cdn.example.com): " CDN_URL
+
+if [ -z "$CDN_URL" ]; then
+  echo "CDN URL cannot be empty."
+  exit 1
+fi
+
+if [[ ! $CDN_URL =~ ^https://.* ]]; then
+  echo "CDN URL must start with https://."
+  exit 1
+fi
+
+read -p "Enter the issued certificate ARN for the CDN URL in the same region as the api-gw (e.g. arn:aws:acm:us-east-1:123:certificate/123-123-123-123): " CERTIFICATE_ARN
+
+CERTIFICATE_DETAILS=$(aws acm describe-certificate --certificate-arn "$CERTIFICATE_ARN" --region "$AWS_DEFAULT_REGION" 2>/dev/null)
+
+if [ $? -ne 0 ]; then
+  echo "Certificate ARN is invalid."
+  exit 1
+fi
+
+VALIDATION_STATUS=$(echo "$CERTIFICATE_DETAILS" | grep -o '"ValidationStatus": *"[^"]*"' | sed 's/"ValidationStatus": *//;s/"//g')
+
+if [ "$VALIDATION_STATUS" != "SUCCESS" ]; then
+  echo "Validation status is not SUCCESS."
+  exit 1
+fi
+
+RAW_CDN_URL=$(echo $CDN_URL | sed 's/https:\/\///')
 
 # Install node modules
 echo "Installing Node.js dependencies..."
@@ -44,4 +72,10 @@ sam deploy --template-file template.yml \
   --capabilities CAPABILITY_IAM \
   --resolve-s3 \
   --region "$AWS_DEFAULT_REGION" \
-  --parameter-overrides DiscordBotToken=$DISCORD_BOT_TOKEN DiscordChannelId=$DISCORD_CHANNEL_ID CDNUrl=$CDN_URL
+  --parameter-overrides DiscordBotToken=$DISCORD_BOT_TOKEN DiscordChannelId=$DISCORD_CHANNEL_ID CDNUrl=$CDN_URL RawCdnUrl=$RAW_CDN_URL CdnUrlCertificateArn=$CERTIFICATE_ARN
+
+API_GW_CNAME=$(aws apigateway get-domain-names --query "items[?domainName=='$RAW_CDN_URL'].regionalDomainName" --output text --region "$AWS_DEFAULT_REGION")
+
+echo "Make sure to update your DNS settings to point: $RAW_CDN_URL to the following CNAME: $API_GW_CNAME"
+
+
